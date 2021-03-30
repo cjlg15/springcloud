70e55cb9945167d25424c08fec64ae4d7f140903
cjlg15 <vxfm219byc0052@126.com>
2021-03-30 17:59:31 +0800

    新增springcloud config

--- springcloud-config/src/main/java/com/liao/springcloudconfig/SpringcloudConfigApplication.java
+++ springcloud-config/src/main/java/com/liao/springcloudconfig/SpringcloudConfigApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@org.springframework.cloud.config.server.EnableConfigServer
 public class SpringcloudConfigApplication {
 
     public static void main(String[] args) {
